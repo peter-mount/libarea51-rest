6feccf2f039b5c795517aac280512c5c9c768ad5
peter <peter@europa>
2016-08-15 11:18:32 +0100

    Don't reset the charbuffer when creating a list

--- lib/rest/append_list.c
+++ lib/rest/append_list.c
@@ -1,19 +1,22 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include <area51/charbuffer.h>
 #include <area51/list.h>
 #include <area51/rest.h>
 
 void json_append_list(struct charbuffer *b, struct List *l, void (*appender)(struct charbuffer *, struct Node *)) {
-    charbuffer_reset(b);
+    bool sep = false;
     charbuffer_add(b, '[');
 
     if (l) {
         struct Node *n = list_getHead(l);
         while (n && list_isNode(n)) {
-            if (b->pos > 1)
+            if (sep)
                 charbuffer_add(b, ',');
+            else
+                sep = true;
             appender(b, n);
             n = n->n_succ;
         }
